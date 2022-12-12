3073758af4897128b3594c490ceb1cc140f10f1c
batuhangoren99@hotmail.com <batuhangoren99@hotmail.com>
2022-12-12 23:04:12 +0300

    Fixed entity bug

---
+++ src/components/Contacts/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Form from './Form'
+import List from './List'
+
+function Contacts() {
+
+  return (
+    <div>
+
+      <div id="container">
+        <h1>Contacts</h1>
+        <Form />
+        <List />
+      </div>
+    </div>
+
+  )
+}
+
+export default Contacts
