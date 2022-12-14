56b8140beef526864fa5971613383bee22b9b3f2
batuhangoren99@hotmail.com <batuhangoren99@hotmail.com>
2022-12-14 03:29:17 +0300

    Added Router, edit page and delete function

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -4,14 +4,18 @@ import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
 export const contactAdaptor = createEntityAdapter();
 const initialState = contactAdaptor.getInitialState();
 
-export const contactSelectors = contactAdaptor.getSelectors(state => state.contacts);
+export const contactSelectors = contactAdaptor
+    .getSelectors(state => state.contacts)
+
 const contactSlice = createSlice({
-    name: "contact",
+    name: "contacts",
     initialState,
     reducers: {
         addContact: contactAdaptor.addOne,
+        deleteContact: contactAdaptor.removeOne,
+        removeAllContact: contactAdaptor.removeAll,
     },
 });
 
-export const { addContact } = contactSlice.actions;
+export const { addContact, deleteContact, removeAllContact } = contactSlice.actions;
 export default contactSlice.reducer;
